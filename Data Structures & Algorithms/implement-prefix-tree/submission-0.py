3bedb935cc0107bc9779ba81e07acf74cc29298f
Tarun Satish Bagewadi <68679605+tabs929@users.noreply.github.com>
2026-06-30 11:46:48 -0700

    Add: implement-prefix-tree - submission-0

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-0.py
@@ -0,0 +1,32 @@
+class PrefixTree:
+
+    def __init__(self):
+        self.children = {}
+        self.word = False
+
+    def insert(self, word: str) -> None:
+        root = self
+        for char in word:
+            if char not in root.children:
+                root.children[char] = PrefixTree()
+            root = root.children[char]
+        root.word = True
+
+    def search(self, word: str) -> bool:
+        root = self
+        for char in word:
+            if char not in root.children:
+                return False
+            root = root.children[char]
+        return root.word
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        root = self
+        for char in prefix:
+            if char not in root.children:
+                return False
+            root = root.children[char]
+        return True
+        
+        
